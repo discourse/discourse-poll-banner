93e6d97c5a5df8f3bbc111ccaacee825e7177bf5
David Taylor <david@taylorhq.com>
2024-03-27 17:44:00 +0000

    DEV: Update linting

--- javascripts/discourse/components/poll-banner.js
+++ javascripts/discourse/components/poll-banner.js
@@ -1,7 +1,7 @@
 import Component from "@ember/component";
+import { action } from "@ember/object";
 import { ajax } from "discourse/lib/ajax";
 import PostCooked from "discourse/widgets/post-cooked";
-import { action } from "@ember/object";
 
 export default Component.extend({
   classNameBindings: ["poll-banner"],
@@ -12,15 +12,18 @@ export default Component.extend({
   init() {
     this._super(...arguments);
 
-    if (!this.currentUser || localStorage.getItem(`polls_${settings.topic_id}_closed`)) {
+    if (
+      !this.currentUser ||
+      localStorage.getItem(`polls_${settings.topic_id}_closed`)
+    ) {
       return;
     }
 
     const date = Date.now();
     let topicId = settings.topic_id;
     let getLocal = localStorage.getItem("polls_" + topicId);
-    let showTime = 60000 * settings.show_after; 
-    let stopTime = 60000 * settings.stop_after; 
+    let showTime = 60000 * settings.show_after;
+    let stopTime = 60000 * settings.stop_after;
     let timeElapsed = 0;
 
     if (topicId && !getLocal) {
@@ -32,23 +35,32 @@ export default Component.extend({
           postId: response.post_stream.posts[0].id,
         });
 
-        localStorage.setItem("polls_" + settings.topic_id, JSON.stringify(cachedTopic));
+        localStorage.setItem(
+          "polls_" + settings.topic_id,
+          JSON.stringify(cachedTopic)
+        );
         this.set("postId", response.post_stream.posts[0].id);
         this.set("cooked", cachedTopic.attrs.cooked);
-      }); 
+      });
     } else if (topicId && getLocal) {
-      let storage = JSON.parse(localStorage.getItem("polls_" + settings.topic_id));
+      let storage = JSON.parse(
+        localStorage.getItem("polls_" + settings.topic_id)
+      );
       timeElapsed = date - storage.attrs.timestamp;
       this.set("cooked", storage.attrs.cooked);
       this.set("postId", storage.attrs.postId);
-    } 
+    }
 
-    if ((timeElapsed >= showTime && timeElapsed <= stopTime)) {
+    if (timeElapsed >= showTime && timeElapsed <= stopTime) {
       this.set("showPoll", true);
-      document.querySelector(".poll-banner-connector").classList.add("visible-poll");
+      document
+        .querySelector(".poll-banner-connector")
+        .classList.add("visible-poll");
     } else {
       this.set("showPoll", false);
-      document.querySelector(".poll-banner-connector").classList.remove("visible-poll");
+      document
+        .querySelector(".poll-banner-connector")
+        .classList.remove("visible-poll");
     }
   },
 
@@ -77,7 +89,7 @@ export default Component.extend({
         },
       }).then((result) => {
         if (result.vote[0].length > 0) {
-          var msDelay = 1000;
+          let msDelay = 1000;
           setTimeout(() => {
             this.send("closePoll");
           }, msDelay);
